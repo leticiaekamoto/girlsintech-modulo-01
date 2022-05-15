a33220b4c4864141b0cb2f87011e85e7feb322ac
leticiaekamoto <sayuriekamoto@gmail.com>
2022-05-15 19:21:19 -0300

    Melhorias no código com alterações nos tipos de saídas das funções

--- funcoes.js
+++ funcoes.js
@@ -1,102 +1,94 @@
-const dados = require('fs').readFileSync('pedidos.json', 'utf-8')
-let arquivo = JSON.parse(dados)
+const dados = require('fs').readFileSync('pedidos.json', 'utf-8');
+let arquivo = JSON.parse(dados);
 
-let itens = []
-const regex = /(item_.)/;
-for(let i = 0; i<arquivo.length;i++){
-    let obj = Object.entries(arquivo[i])
-        obj.map(key => {
-        if(regex.test(key)){
-            let item = key.join()
-            itens.push(item)               
-       } 
-    })  
+const regexItem = /(item_.)/;
+const regexMerchant = /(merchant_.)/;
+const regexCustomer = /(customer_.)/;
+
+function retornarEntidade(regex, arquivo){
+    const entidade = [];
+    for(let i = 0; i<arquivo.length;i++){
+        let obj = arquivo[i];
+        let temporaria = {};
+        Object.keys(obj).forEach(key => {
+            if(regex.test(key)){
+                temporaria[key] = obj[key]
+            };
+        });
+        entidade.push(temporaria);
+    }
+    return entidade;
 }
 
-let merchant = []
-const regex2 = /(merchant_.)/;
-for(let i = 0; i<arquivo.length;i++){
-    let obj = Object.entries(arquivo[i])
-        obj.map(key => {
-        if(regex2.test(key)){
-            let merchantDados = key.join()
-            merchant.push(merchantDados)               
-       } 
-    })  
-}
-
-let customer = []
-const regex3 = /(customer_.)/;
-for(let i = 0; i<arquivo.length;i++){
-    let obj = Object.entries(arquivo[i])
-        obj.map(key => {
-        if(regex3.test(key)){
-            let customerDados = key.join()
-            customer.push(customerDados)               
-       } 
-    })  
-}
+const itens = retornarEntidade(regexItem, arquivo);
+const merchant = retornarEntidade(regexMerchant, arquivo);
+const customer = retornarEntidade(regexCustomer, arquivo);
 
 function totalPrice(indice){  
-    let {item_quantity, item_unitPrice} = arquivo[indice]
-    const totalPrice = item_quantity * item_unitPrice
-    return totalPrice.toFixed(2)   
+    let {item_quantity, item_unitPrice} = arquivo[indice];
+    let totalPrice = (item_quantity * item_unitPrice).toFixed(2);
+    totalPrice = parseFloat(totalPrice);
+    return totalPrice;
 }
 
 function cartTotal(indice){
-    let {item_discount} = arquivo[indice]
-    const cartTotal = totalPrice(indice) - item_discount
-    return cartTotal
+    let {item_discount} = arquivo[indice];
+    const cartTotal = totalPrice(indice) - item_discount;
+    return cartTotal;
 }
 
 function paymentsValue1(indice){
-    let paymentsValue1 = cartTotal(indice)*0.33
-    return paymentsValue1.toFixed(2)
+    let paymentsValue1 = (cartTotal(indice)*0.33).toFixed(2);
+    paymentsValue1 = parseFloat(paymentsValue1);
+    return paymentsValue1;
 }
 
 function changeFor1(indice){
-    let {'payments_payment[1]_method': metodoPagamento1} = arquivo[indice]
+    let {'payments_payment[1]_method': metodoPagamento1} = arquivo[indice];
     
     if (metodoPagamento1 === "CASH"){
-        pagamento1 = parseFloat(paymentsValue1(indice)) + parseFloat(paymentsValue1(indice)*0.07)
-        
+        pagamento1 = (paymentsValue1(indice) + (paymentsValue1(indice)*0.07)).toFixed(2);
+        pagamento1 = parseFloat(pagamento1);
     }else{
-        pagamento1 = 0
+        pagamento1 = 0;
     }
-    return pagamento1.toFixed(2)
+    return pagamento1;
 }
 
+
 function paymentsValue2(indice){
-    let paymentsValue2 = cartTotal(indice)*0.67
-    return paymentsValue2.toFixed(2)
+    let paymentsValue2 = (cartTotal(indice)*0.67).toFixed(2);
+    paymentsValue2 = parseFloat(paymentsValue2);
+    return paymentsValue2;
 }
 
 function changeFor2(indice){
-    let {'payments_payment[2]_method': metodoPagamento2} = arquivo[indice]
+    let {'payments_payment[2]_method': metodoPagamento2} = arquivo[indice];
     if (metodoPagamento2 === "CASH"){
-        pagamento2 = parseFloat(paymentsValue2(indice)) + parseFloat(paymentsValue2(indice)*0.11)
+        pagamento2 = (paymentsValue2(indice) + paymentsValue2(indice)*0.11).toFixed(2);
+        pagamento2 = parseFloat(pagamento2);
     }else{
-        pagamento2 = 0
+        pagamento2 = 0;
     }
-    return pagamento2.toFixed(2)
+    return pagamento2;
 }
 
 function totalInCash(indice){
-    let {'payments_payment[1]_method': metodoPagamento1} = arquivo[indice]
-    let {'payments_payment[2]_method': metodoPagamento2} = arquivo[indice]
-    let totalEmDinheiro = 0
+    let {'payments_payment[1]_method': metodoPagamento1} = arquivo[indice];
+    let {'payments_payment[2]_method': metodoPagamento2} = arquivo[indice];
+    let totalEmDinheiro = 0;
     if(metodoPagamento1 === "CASH"){
-        totalEmDinheiro = totalEmDinheiro + parseFloat(paymentsValue1(indice))
+        totalEmDinheiro = totalEmDinheiro + paymentsValue1(indice);
     }
     if(metodoPagamento2 ==="CASH"){
-        totalEmDinheiro = totalEmDinheiro + parseFloat(paymentsValue2(indice))
+        totalEmDinheiro = totalEmDinheiro + paymentsValue2(indice);
     }
-    return totalEmDinheiro
+    return totalEmDinheiro;
 }
 
 function change(indice){
-    let troco = parseFloat(changeFor1(indice)) + parseFloat(changeFor2(indice)) - (parseFloat(totalInCash(indice)))
-    return troco.toFixed(2)
+    let troco = (changeFor1(indice) + changeFor2(indice) - (totalInCash(indice))).toFixed(2);
+    troco = parseFloat(troco);
+    return troco
 }
 
-console.log(itens)
